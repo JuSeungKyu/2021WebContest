0398f579c0f482e11bc7e9ce0ce3a3f055dceebb
JuSeungKyu <juseungkyu0711@gmail.com>
2021-07-07 00:26:41 +0900

    서브페이지2 스크롤 이벤트 추가

---
+++ sub2/script.js
@@ -0,0 +1,34 @@
+function scrool_check() {
+    let web_height = document.querySelector('body').clientHeight;
+    let scroll_bottom = document.querySelector('html').scrollTop + window.innerHeight
+    
+    let scroll = scroll_bottom / web_height
+    if(scroll >= 0.84){
+        clearInterval(interval)
+        section_in()
+    }
+}
+
+function section_in(){
+    document.querySelectorAll('#numerical h2').forEach(e=>{
+        setTimeout(number_up, 10, e, 1)
+    })
+}
+
+function number_up(e, n){
+    if(n-10 >= e.dataset.number){
+        e.innerHTML = e.dataset.number+'표'
+        return
+    }
+    
+    e.innerHTML = n+'표'
+    // 시간
+    // y= a(x-5000)^2+10
+    // 100 = a(10000-5000)^2+10
+    // 90a = 2,500,000
+    // console.log((9/8000000)*(n-5000)*(n-5000)+10)
+    setTimeout(number_up, Math.round((9/8000000)*(n-5000)*(n-5000)+10), e, n+Math.round(e.dataset.number*40 / (n<5000?5000:n*3)))
+}
+
+
+let interval = setInterval(scrool_check, 100);
